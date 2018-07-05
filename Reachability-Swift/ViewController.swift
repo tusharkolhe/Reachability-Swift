8bdd9421ac81bd6d9c17c0687d3bb8f347eba0c5
“Tushar <tushar.kolhe01@gmail.com>
2018-07-05 17:15:40 +0530

    Here you can check your Internet Connection.

--- Reachability-Swift/ViewController.swift
+++ Reachability-Swift/ViewController.swift
@@ -16,6 +16,27 @@ class ViewController: UIViewController {
        
     }
 
+    @IBAction func checkBtn(_ sender: Any) {
+        if ConnectionCheck.isConnectedToNetwork() {
+            print("Connected")
+            let alertController = UIAlertController(title: "Hey!!", message: "Your internet connection is ONN...", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+        }
+        else{
+            print("disConnected")
+            
+            let alertController = UIAlertController(title: "Alert!!", message: "Please check your internet connection!!!", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
